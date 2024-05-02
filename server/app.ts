7cba0277d209f7db12ec376810a456d7e2c19445
Matija Osrečki <github@matijao.com>
2024-05-02 18:14:13 +0200

    handle deploy to prod

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger';
 import { expensesRoute } from './routes/expenses';
+import { serveStatic } from 'hono/bun';
 
 const app = new Hono()
 
@@ -9,6 +10,10 @@ app.use("*", logger());
 const apiRoutes = app.basePath('/api')
   .get('/', (c) => c.text('Up and running! ✨'))
   .route('/expenses', expensesRoute)
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
+
 
 export default app
 export type ApiRoutes = typeof apiRoutes
